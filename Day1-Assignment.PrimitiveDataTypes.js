08b01e30415dc2e2bdd1b6be124dfc8cfae859b1
pavithra9695 <pavihari1120@gmail.com>
2025-09-22 17:14:47 +0530

    Day1-Assignment.PrimitiveDataTypes.js

---
+++ Day1-Assignment.PrimitiveDataTypes.js
@@ -0,0 +1,20 @@
+
+let firstname = 'Pavithra'
+console.log (firstname)
+console.log (typeof firstname)
+
+
+let companyname = 'TestLeaf';
+console.log(companyname);
+console.log(typeof companyname);
+
+let mobilenumber = 4342678143
+console.log(mobilenumber);
+console.log(typeof mobilenumber);
+
+let isAutomation = true
+console.log(isAutomation);
+console.log(typeof isAutomation );
+
+
+
