f6f12ca2ab8f2384fa1fcc2f3548ee8ca8364b36
pavithra9695 <pavihari1120@gmail.com>
2025-09-22 18:20:38 +0530

    Number_type

---
+++ Number.js
@@ -0,0 +1,20 @@
+
+function numbertype(number)
+{
+    if (number===0){
+        console.log("The number is Neutral")
+    }
+    else if(number>0)
+    {
+        console.log("the number is Positive")
+
+    }
+    else if(number<0)
+    {
+        console.log("The number is Negative")
+    }
+    else
+    {console.log("check your input")}
+
+}
+numbertype(10)
