aafa3f06b627518ce5c731d0fef78ab612a3130f
pavithra9695 <pavihari1120@gmail.com>
2025-09-23 16:38:10 +0530

    Odd or Even

---
+++ Oddoeeven.js
@@ -0,0 +1,16 @@
+
+function isOddOrEven (number)
+    {
+        if(number%2===1)
+        {
+            console.log("oddnumber");
+        }
+        else
+        {
+            console.log("evennumber");
+        }
+            
+    }
+isOddOrEven()
+
+
